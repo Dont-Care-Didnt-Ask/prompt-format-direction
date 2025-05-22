25ceb4ca6c9187354df646dbc41a5f755ba943c0
Seleznev Mikhail <seleznev@airi.net>
2025-05-23 00:33:23 +0300

    Factor out selecting few shot examples

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import argparse
 import json
 import torch
 import os
-from typing import List
-from datasets import load_dataset
+from typing import List, Dict
+from datasets import load_dataset, Dataset
 from functools import partial
 from tqdm.auto import tqdm
 
@@ -28,6 +28,22 @@ def _save_json(data, path):
     with open(path, "w") as f:
         json.dump(data, f, indent=2)
 
+def select_few_shot_examples(train_dataset: Dataset, seed: int, subset_size: int, print_debug_info: bool) -> List[Dict]:
+    """Selects two examples from a random subset training dataset with the shortest and longest solutions.
+    We order the examples by solution length to create a progression from easier to harder examples.
+    We select examples from a subset of the training dataset to avoid choosing too outlier-like examples.
+    """
+    train_dataset = train_dataset.shuffle(seed=seed).select(range(subset_size))
+    solution_lengths = [len(example["answer"]) for example in train_dataset]
+    shortest_solution_index = _argmin(solution_lengths)
+    longest_solution_index = _argmax(solution_lengths)
+
+    if print_debug_info:
+        print(f"Sample with shortest solution:\n{train_dataset[shortest_solution_index]}")
+        print(f"Sample with longest solution:\n{train_dataset[longest_solution_index]}")
+
+    return [train_dataset[shortest_solution_index], train_dataset[longest_solution_index]]
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model-name-or-path", type=str, required=True)
@@ -37,6 +53,9 @@ def parse_args():
     parser.add_argument("--root-dir", type=str, default="exp")
     parser.add_argument("--n-format-specs", type=int, default=10)
     parser.add_argument("--seed", type=int, default=24)
+    # In GSM8K, the reasoning and answer are both contained
+    # in column "answer" as a string and are separated by "####"
+    parser.add_argument("--reasoning-answer-separator", type=str, default="####")
     return parser.parse_args()
 
 def main():
@@ -69,20 +88,8 @@ def main():
     
     test_dataset.to_json(os.path.join(experiment_dir, "sorted_test_gsm8k_subset.jsonl"))
 
-    print("Train dataset:")
-    print(train_dataset[:10])
-    print("Test dataset:")
-    print(test_dataset[:10])
-
     # Select few-shot examples
-    train_dataset = train_dataset.shuffle(seed=args.seed).select(range(100))
-    solution_lengths = [len(example["answer"]) for example in train_dataset]
-    shortest_solution_index = _argmin(solution_lengths)
-    longest_solution_index = _argmax(solution_lengths)
-    print(f"Sample with shortest solution:\n{train_dataset[shortest_solution_index]}")
-    print(f"Sample with longest solution:\n{train_dataset[longest_solution_index]}")
-
-    few_shot_examples = [train_dataset[shortest_solution_index], train_dataset[longest_solution_index]]
+    few_shot_examples = select_few_shot_examples(train_dataset, args.seed, 100, print_debug_info=True)
 
     # Generate format specifications
     format_specs: List[FormatSpecification] = sample_format_specs_with_fixed_descriptors(args.n_format_specs, args.seed,
@@ -99,7 +106,7 @@ def main():
                 build_gsm8k_few_shot_prompt,
                 few_shot_examples=few_shot_examples,
                 format_spec=format_spec,
-                reasoning_answer_separator="####",
+                reasoning_answer_separator=args.reasoning_answer_separator,
             )
 
             generations = get_generations(test_dataset, prompt_builder_fn, model, tokenizer, device, stop_strings, batch_size=batch_size)
