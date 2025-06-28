e2985b6094751f41129be186873abed385f526d5
Seleznev Mikhail <seleznev@airi.net>
2025-06-28 16:45:08 +0300

    Plot Llama and Qwen models

--- plot.py
+++ plot.py
@@ -8,17 +8,25 @@ sns.set_theme(style="darkgrid", font_scale=1.4)
 
 def main():
     experiment_dirs = [
-        "exp/qwen1b_deformatted",
+        # "exp/qwen1b_deformatted",
         "exp/qwen1b",
-        "exp/qwen3b_deformatted",
+        # "exp/qwen3b_deformatted",
         "exp/qwen3b",
+        "exp/Llama-3.2-1B-Instruct",
+        "exp/Llama-3.2-3B-Instruct",
+        "exp/Meta-Llama-3.1-8B-Instruct",
+        "exp/Qwen2.5-7B-Instruct",
     ]
 
     model_names = {
-        "qwen1b_deformatted": "Qwen2.5-1.5B-Deformatted",
+        # "qwen1b_deformatted": "Qwen2.5-1.5B-Deformatted",
         "qwen1b": "Qwen2.5-1.5B",
-        "qwen3b_deformatted": "Qwen2.5-3B-Deformatted",
+        # "qwen3b_deformatted": "Qwen2.5-3B-Deformatted",
         "qwen3b": "Qwen2.5-3B",
+        "Qwen2.5-7B-Instruct": "Qwen2.5-7B",
+        "Llama-3.2-1B-Instruct": "Llama-3.2-1B",
+        "Llama-3.2-3B-Instruct": "Llama-3.2-3B",
+        "Meta-Llama-3.1-8B-Instruct": "Meta-Llama-3.1-8B",
     }
 
 
@@ -44,35 +52,52 @@ def main():
 
     dpi = 400
 
-    plt.figure(figsize=(7, 4))
-    sns.stripplot(x="accuracy", y="model", data=df)
-    plt.xlabel("Accuracy")
-    plt.ylabel("")
-    plt.title("Distribution of accuracy over 10 formats")
-    plt.savefig("images/accuracy_distribution_per_model.png", dpi=dpi, bbox_inches="tight")
-    plt.close()
-
     df["model"] = pd.Categorical(
         df["model"],
-        categories=["Qwen2.5-1.5B", "Qwen2.5-1.5B-Deformatted", "Qwen2.5-3B", "Qwen2.5-3B-Deformatted"],
+        categories=model_names.values(),
+        #["Qwen2.5-1.5B", "Qwen2.5-1.5B-Deformatted", "Qwen2.5-3B", "Qwen2.5-3B-Deformatted"],
         ordered=True,
     )
 
-    sns.barplot(x="model", y="accuracy", data=df, errorbar="sd")
+    def get_family(model_name):
+        if "Llama" in model_name:
+            return "Llama"
+        elif "Qwen" in model_name:
+            return "Qwen"
+        else:
+            raise ValueError(f"Unknown model family: {model_name}")
+
+    df["family"] = df["model"].apply(get_family)
+
+    plt.figure(figsize=(7, 4))
+    ax = sns.stripplot(x="accuracy", y="model", data=df, hue="family")
+    plt.xlabel("Accuracy")
+    plt.ylabel("")
+    plt.title("Distribution of accuracy over 10 formats")
+    ax.get_legend().remove()
+    plt.savefig("images/accuracy_distribution_per_model.png", dpi=dpi, bbox_inches="tight")
+    plt.close()
+
+    plt.figure(figsize=(9, 4))
+    ax = sns.barplot(x="model", y="accuracy", data=df, errorbar="sd", hue="family")
     plt.xlabel("")
     plt.ylabel("Accuracy", labelpad=20)
     plt.title("Performance")
     plt.xticks(rotation=20, ha="right")
+    ax.get_legend().remove()
     plt.savefig("images/accuracy_per_model.png", dpi=dpi, bbox_inches="tight")
     plt.close()
 
-    df_grouped = df.groupby("model").agg({"accuracy": lambda x: x.max() - x.min()}).reset_index()
+    df_grouped = df.groupby("model").agg({"accuracy": lambda x: x.max() - x.min(), "family": lambda x: x.iloc[0]}).reset_index()
+    print(df_grouped)
 
-    sns.barplot(x="model", y="accuracy", data=df_grouped)
+    plt.figure(figsize=(9, 4))
+    ax = sns.barplot(x="model", y="accuracy", data=df_grouped, hue="family")
     plt.xlabel("")
     plt.title("Sensitivity\n(lower is better)")
     plt.ylabel("Spread of accuracy over 10 formats", labelpad=20)
     plt.xticks(rotation=20, ha="right")
+    ax.get_legend().remove()
     plt.savefig("images/spread_accuracy_per_model.png", dpi=dpi, bbox_inches="tight")
     plt.close()
 
