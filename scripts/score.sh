bfed23499152c0ff3d90ac5c419dc3b9cdce57af
Seleznev Mikhail <seleznev@airi.net>
2025-12-01 10:55:49 +0300

    Update scoring script

--- scripts/score.sh
+++ scripts/score.sh
@@ -1,5 +1,15 @@
-for experiment_name in Llama-3.2-1B-Instruct Llama-3.2-3B-Instruct Meta-Llama-3.1-8B-Instruct Qwen2.5-7B-Instruct
+devices=0
+
+# for experiment_name in Llama-3.2-1B-Instruct Llama-3.2-3B-Instruct Meta-Llama-3.1-8B-Instruct Qwen2.5-7B-Instruct
+# do
+#     echo "Scoring $experiment_name"
+#     python evaluate.py -e exp/$experiment_name
+# done
+
+
+for experiment_name in Llama-3.2-1B-Instruct_lora Llama-3.2-3B-Instruct_lora Meta-Llama-3.1-8B-Instruct_lora Qwen2.5-7B-Instruct_lora Qwen2.5-1.5B-Instruct_lora Qwen2.5-3B-Instruct_lora
 do
     echo "Scoring $experiment_name"
-    python evaluate.py -e exp/$experiment_name
+    echo "Using $(which python)"
+    CUDA_VISIBLE_DEVICES=$devices python evaluate.py -e exp/$experiment_name
 done
